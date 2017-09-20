7388deae6397bce63ed8996390c2a648596965d7
pinko <agaslota@wp.pl>
2017-09-20 13:59:46 +0100

    banner-technical test for SkyWorks

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+$(document).ready(function() {
+
+	$('#js-frameWithBackground').css("display","none").delay(1000).fadeIn(1000);
+    $('#js-slogan').css("display","none").delay(1000).fadeIn(1000);
+    $('#js-products').css("display","none").delay(2000).fadeIn(1000);
+    $('#js-logo').css("display","none").delay(2500).fadeIn(1000);
+    $('#js-buttonMore').slideUp(1000).delay( 4000 ).fadeIn(2000).css("background-color","#f1432a");
+});
